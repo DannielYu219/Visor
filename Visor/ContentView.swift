70d2c40d0ed7cedc3a968b5658676b5c2bfccba1
DannielYu <yubolinqwer@163.com>
2026-07-03 17:40:31 +0800

    Initial Commit

---
+++ Visor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Visor
+//
+//  Created by Danniel Yu on 7/3/R8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visor/VisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisorApp.swift
+//  Visor
+//
+//  Created by Danniel Yu on 7/3/R8.
+//
+
+import SwiftUI
+
+@main
+struct VisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
